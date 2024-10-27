9dcafe87b5a49f9a6727e1f155efdd786d625c3b
ninafleur21 <ninabiamou27@gmail.com>
2024-10-27 16:51:41 +0100

    Add files via upload

---
+++ rectangle.js
@@ -0,0 +1,10 @@
+const prompt = require('prompt-sync')();
+
+const longueur = parseFloat(prompt("Entrez la longueur du rectangle : "));
+const largeur = parseFloat(prompt("Entrez la largeur du rectangle : "));
+
+const perimetre = 2 * (longueur + largeur);
+const aire = longueur * largeur;
+
+console.log(`Le périmètre du rectangle est : ${perimetre}`);
+console.log(`L'aire du rectangle est : ${aire}`);
